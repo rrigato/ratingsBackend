1c018ed7219acd920c04abe921f82f7c95a09616
Ryan <rrigato@horizon.csueastbay.edu>
2017-07-30 14:53:16 -0500

    Able to check if there are any tables in DynamoDb

--- ratingsCreation.py
+++ ratingsCreation.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import boto3
 class ratings:
 	'''
 		Loads the json file and creates a DynamoDB table to upload it
@@ -15,7 +16,23 @@ class ratings:
 		'''
 		self.train = pd.read_json('/home/ryan/Documents/ratingsBackend/toonamiCombindedDate.json')
 		print(self.train.head())
+
+	def awsConnect(self):
+		'''
+			Configures the connection to AWS
+		'''
+		#self.dynamodb = boto3.client('dynamodb', endpoint_url = 'https://dynamodb.us-east-1.amazonaws.com')
+		self.dynamodb = boto3.client('dynamodb')
+		#self.dynamodb = boto3.client('dynamodb', endpoint_url = 'http://localhost:8000')
+		print(self.dynamodb.list_tables())
 		
 if __name__ == '__main__':
 	ratingsObj = ratings()
+
+	ratingsObj.awsConnect()
 	
+
+
+
+
+#AttributeName=Show,AttributeType=S AttributeName=Date,AttributeType=S AttributeName=Time,AttributeType=S AttributeName=Household,AttributeType=S AttributeName=Total,AttributeType=S AttributeName=AHousehold,AttributeType=S AttributeName=ATotal,AttributeType=S --key-schema AttributeName=Show,KeyType=HASH AttributeName=Date,KeyType=RANGE --provisioned-throughput ReadCapacityUnits=5,WriteCapacityUnits=5
